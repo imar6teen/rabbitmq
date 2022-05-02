310f18cf989dbe8aa4b9466e12f8d9edf3b54023
Aimar Anand <aimaranand02@gmail.com>
2022-05-02 10:01:20 +0800

    end of learning publish/subscribe

--- receive.py
+++ receive.py
@@ -1,5 +1,7 @@
 #! python
 
+from ast import ExceptHandler
+import queue
 import pika
 import sys
 import os
@@ -12,23 +14,27 @@ def main():
 
     channel = connection.channel()
 
-    channel.queue_declare("task_queue", durable=True)
+    # channel.queue_declare("task_queue", durable=True)
+    channel.exchange_declare(exchange="logs", exchange_type='fanout')
+
+    result = channel.queue_declare(queue="", exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(queue=queue_name, exchange='logs')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body.decode('utf-8'))
         time.sleep(body.count(b'.'))
-        time.sleep(int(sys.argv[1]) if sys.argv[1] else 0)
         print(" [x] done")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
 
     print("Connected to rabbitmq")
 
     # FAIR DISPATCH
     # Make rabbitmq send to other consumer when the consumer busy
     # rather than send to consumer fairly
-    channel.basic_qos(prefetch_count=1)
+    # channel.basic_qos(prefetch_count=1)
 
-    channel.basic_consume(queue="task_queue", auto_ack=False,
+    channel.basic_consume(queue=queue_name, auto_ack=True,
                           on_message_callback=callback)
 
     channel.start_consuming()
--- send.py
+++ send.py
@@ -7,7 +7,10 @@ connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 
 channel = connection.channel()
 
-channel.queue_declare(queue="task_queue", durable=True)
+# channel.queue_declare(queue="task_queue", durable=True)
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
 
 message = ' '.join(sys.argv[1:2])
 
@@ -15,12 +18,17 @@ message = ' '.join(sys.argv[1:2])
 for i in range(int(sys.argv[2])):
     dots = '.' * i
     load_message = message + dots
+    # channel.basic_publish(
+    #     exchange="",
+    #     routing_key="task_queue",
+    #     body=load_message,
+    #     properties=pika.BasicProperties(
+    #         delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE)
+    # )
     channel.basic_publish(
-        exchange="",
-        routing_key="task_queue",
+        exchange="logs",
+        routing_key="",
         body=load_message,
-        properties=pika.BasicProperties(
-            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE)
     )
     print(f"[x] sent {load_message}")
 
